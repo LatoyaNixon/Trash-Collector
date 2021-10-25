a5d7b5f55f14471e15770298f9555f223ee3f35b
Sean May <s_may318@hotmail.com>
2021-10-25 15:47:27 -0400

    added the index function to the employees app.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.apps import apps
+from datetime import date
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Employees
 
 # Create your views here.
 
@@ -10,4 +14,19 @@ from django.apps import apps
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employees = Employees.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employees': logged_in_employees,
+            'today': today
+        }
+        return render(request, 'employees/index.html')
+    except ObjectDoesNotExist:
+            return HttpResponseRedirect(reverse('employees:create'))
+    
