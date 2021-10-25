f8a384594399dba4ac282494ab48f65fcbbd9f71
Sean May <s_may318@hotmail.com>
2021-10-25 17:41:17 -0400

    Working on displaying customers that hold the same zipcode as my employee so they know who is in there are for pick up.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.apps import apps
 from datetime import date
 from django.core.exceptions import ObjectDoesNotExist
+
 from .models import Employees
 
 # Create your views here.
@@ -14,17 +15,20 @@ from .models import Employees
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
+    
     # The following line will get the logged-in user (if there is one) within any view function
     logged_in_user = request.user
     try:
         # This line will return the customer record of the logged-in user if one exists
-        logged_in_employees = Employees.objects.get(user=logged_in_user)
-
+        logged_in_employee = Employees.objects.get(user=logged_in_user)
+        customer_zip_code = Customer.objects.filter(zip_code=logged_in_employee.zip_code) 
+        
         today = date.today()
         
         context = {
-            'logged_in_employees': logged_in_employees,
-            'today': today
+            'logged_in_employee': logged_in_employee,
+            'today': today,
+            'customer_zip_code': customer_zip_code
         }
         return render(request, 'employees/index.html')
     except ObjectDoesNotExist:
